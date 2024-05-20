8e6670dc3420f9e2465f8503c04a0ea333aa3255
JohariSahil <sahiljohari04@gmail.com>
2024-05-20 21:21:46 +0530

    fix for imer

--- src/components/FetchItems.jsx
+++ src/components/FetchItems.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { itemsActions } from "../store/itemSlice";
+import { fetchStatusActions } from "../store/fetchStatusSlice";
 
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
@@ -8,21 +9,19 @@ const FetchItems = () => {
 
   useEffect(() => {
     if (fetchStatus.fetchDone) return;
+
+    dispatch(fetchStatusActions.markFetchingStarted());
     fetch("http://localhost:8080/items")
       .then((res) => res.json())
       .then(({ items }) => {
+        dispatch(fetchStatusActions.markFetchDone());
+        dispatch(fetchStatusActions.markFetchingFinished());
         dispatch(itemsActions.addInitialItems(items[0]));
       });
+    return () => {};
   }, [fetchStatus]);
 
-  return (
-    <>
-      <div>
-        Fetch Done:{fetchStatus.fetchDone}
-        Currently Fetching: {fetchStatus.currentlyFetching}
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default FetchItems;
